7e9eaad6301ccaaf1823a8ef26dca1a7d70c4396
Prem Prasad <premrinkinoniya@gmail.com>
2025-12-05 19:47:44 +0530

    updating commit

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-module.exports= function dbconnect(){
-  const uri=process.env.MONGO_URI;
-  if(!uri){
-    console.errpr('MONGO_URI not set in .en');
+module.exports = function dbconnect() {
+  const uri = process.env.MONGO_URI;
+
+  if (!uri) {
+    console.error('MONGO_URI not set in .env');   // FIXED
     process.exit(1);
-
   }
 
-  mongoose.connect(uri).then(()=>console.log('MongoDb Connected'))
-  .catch((err)=>{
-    console.error('mongoDb connectio error',err.message || err);
-    process.exit(1);
-  })
-  
+  mongoose
+    .connect(uri)
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => {
+      console.error('MongoDB connection error:', err.message || err); // FIXED LOG
+      process.exit(1);
+    });
 };
